904059c926720402041c53d8a4242621309c1bb0
phcm-desktop <phchavesmaia@gmail.com>
2026-09-23 17:18:16 -0300

    Add real IBGE conversion integration tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
 using TestItemRunner
 
-@run_package_tests verbose=true
+@run_package_tests verbose=true filter=ti -> (
+  !(:integration in ti.tags) || get(ENV, "CENSOBR_INTEGRATION_TESTS", "false") == "true"
+)
